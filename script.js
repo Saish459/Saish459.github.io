c2ca2a81570993bb4a79f1fff12c59d526bce858
Pavan Saish <89860317+saish2003@users.noreply.github.com>
2023-07-03 01:05:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// Smooth scrolling functionality
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+});
+
+var typed = new Typed('#typing-text', {
+    strings: ["I'm Pavan Saish..."],
+    typeSpeed: 120,
+    backSpeed: 50,
+    startDelay: 500,
+    showCursor: false,
+    loop: true
+  });
+  
+  
